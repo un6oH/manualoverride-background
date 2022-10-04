a85252c3b758c929d266787c365f19fa7cae9ed9
un6oy <un6oy.hogun@gmail.com>
2022-10-04 21:47:30 +1000

    initial commit

---
+++ sketch.js
@@ -0,0 +1,34 @@
+let theta = 0;
+let phi = 0;
+const delta = 20;
+const period = 2;
+const numDots = 8;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  colorMode(HSB, 360, 100, 100, 100);
+}
+
+function draw() {
+  background(0, 0, 100);
+
+  push();
+  translate(width / 2, height / 2);
+  rotate(phi);
+  noStroke();
+  for (let i = 0; i < numDots; i++) {
+    let a = theta + TWO_PI / numDots * i;
+    let b = a + sin(a);
+    fill(360 / numDots * i, 100, 100);
+    ellipse(cos(b) * 50, sin(b) * 50, 20, 20);
+  }
+  pop();
+
+  theta = theta < TWO_PI ? theta + TWO_PI / frameRate() / period : 0;
+  phi = phi < TWO_PI ? phi + TWO_PI / frameRate() / 5 : 0;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
